7df70bacbbaef944f765e87dd1f23d12d7fea249
Jérémy Magnan <jeremy@magnan.one>
2019-06-29 19:48:11 +0200

    Ajout d'un script de syncro

--- update-folder.sh
+++ update-folder.sh
@@ -4,6 +4,7 @@
 
 rsync -v -r --delete-after ~/.config/i3/ .config/i3/
 rsync -v -r --delete-after ~/.config/polybar/ .config/polybar/
+rsync -v -r --delete-after ~/.config/compton/ .config/compton/
 rsync -v -r --delete-after ~/.Xdefaults .
 rsync -v -r --delete-after ~/.zshrc .
 rsync -v -r --delete-after ~/.face .
