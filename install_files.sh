df503767b51defbd2567f8526c876c2879b224bc
Jérémy Magnan <jeremy@magnan.one>
2019-08-12 11:01:26 +0200

    Raccourcie pour la configuration + workspace pour la virtualisation

---
+++ install_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Require rsync
+
+rsync -v -r --delete-after .config/i3/ ~/.config/i3/ 
+rsync -v -r --delete-after .config/polybar/ ~/.config/polybar/ 
+rsync -v -r --delete-after .config/compton/ ~/.config/compton/ 
+rsync -v -r --delete-after .config/rofi/ ~/.config/rofi/ 
+rsync -v -r --delete-after .config/neofetch/~/.config/neofetch/ 
+rsync -v -r --delete-after .vim/ ~/.vim/ 
+rsync -v -r --delete-after .Xdefaults ~/.Xdefaults 
+rsync -v -r --delete-after .zshrc  ~/.zshrc 
+rsync -v -r --delete-after .face ~/.face 
+rsync -v -r --delete-after .vimrc ~/.vimrc 
+rsync -v -r --delete-after .xinitrc ~/.xinitrc 
